94dc723bc7402764d4b516ae45db0cc0fd9cc339
SeungGukYoo <dbtmd878@gmail.com>
2023-10-15 15:05:22 +0900

    feat: 날짜 선택 팝업 기능 구현

--- src/routes/reservation/Popup.svelte
+++ src/routes/reservation/Popup.svelte
@@ -4,13 +4,100 @@
 	import upArrowIcon from '$lib/images/icons/chevron-up.svg';
 	import dateIcon from '$lib/images/icons/today.svg';
 	import deleteIcon from '$lib/images/icons/trash.svg';
-	import { handlePopUp } from '../../store';
+	import { get } from 'svelte/store';
+	import { handlePopUp, handleReservationInfo } from '../../store';
+	type Calender = {
+		[key: string]: number;
+	};
 
+	const period = ['AM', 'PM'];
+	const calendar: Calender = {
+		Jen: 31,
+		Feb: 28,
+		Mar: 31,
+		Apr: 30,
+		May: 31,
+		Jun: 30,
+		Jul: 31,
+		Aug: 31,
+		Sep: 30,
+		Oct: 31,
+		Nov: 30,
+		Dec: 31
+	};
+	let timeFocus = false;
+	let dateFocus = false;
+	let maxDay = Array.from({ length: 30 }, (_, idx) => idx + 1);
+	let currentHour = '07';
+	let currentMinute = '00';
+	let currentTimePeriod = 0;
+	let choiceMonth: string = 'Sep';
+	let choiceDay: number = 1;
+	let isSaveDate = false;
+	$: fullDate =
+		choiceMonth +
+		' ' +
+		choiceDay +
+		',' +
+		currentHour +
+		':' +
+		currentMinute +
+		period[currentTimePeriod];
 	function closePopUp() {
 		handlePopUp.update((prev) => {
 			prev = false;
 			return prev;
 		});
+	}
+	function changeHour(hour: number) {
+		timeFocus = true;
+		dateFocus = false;
+		let changeHour = parseInt(currentHour) + hour;
+		if (changeHour < 0) changeHour = 24;
+		if (changeHour > 24) changeHour = 0;
+		currentHour = changeHour < 10 ? '0' + changeHour.toString() : changeHour.toString();
+	}
+	function changeMinute(minute: number) {
+		timeFocus = true;
+		dateFocus = false;
+		let changeMinute = parseInt(currentMinute) + minute;
+		if (changeMinute < 0) changeMinute = 59;
+		if (changeMinute > 59) changeMinute = 0;
+		currentMinute = changeMinute < 10 ? '0' + changeMinute.toString() : changeMinute.toString();
+	}
+	function changePeriod(periodIdx: number) {
+		timeFocus = true;
+		dateFocus = false;
+		let changePeriodIdx = currentTimePeriod + periodIdx;
+		if (changePeriodIdx >= period.length) changePeriodIdx = 0;
+		if (changePeriodIdx < 0) changePeriodIdx = 1;
+		currentTimePeriod = changePeriodIdx;
+	}
+	function changeDate(isChangeDate: boolean) {
+		if (!isChangeDate) {
+			timeFocus = false;
+			dateFocus = true;
+			isSaveDate = false;
+		} else {
+			dateFocus = false;
+			isSaveDate = true;
+		}
+	}
+	function makeDays() {
+		if (!choiceMonth) {
+			return;
+		}
+		maxDay = Array.from({ length: calendar[choiceMonth] }, (_, idx) => idx + 1);
+		choiceDay = 1;
+	}
+	function saveDate() {
+		if (!isSaveDate) return;
+		handleReservationInfo.update((prev) => {
+			prev.reservedDate = fullDate;
+			return prev;
+		});
+		console.log(get(handleReservationInfo));
+		handlePopUp.update((prev) => (prev = false));
 	}
 </script>
 
@@ -18,37 +105,69 @@
 	<div class="showDateContainer">
 		<div class="timeContainer">
 			<img src={timeIcon} alt="" />
-			<p class="showInputContainer">2:00 PM</p>
+			<p class="showInputContainer time {timeFocus ? 'on' : 'off'}">
+				<span>{currentHour}</span>
+				:
+				<span>{currentMinute}</span>
+				<span>{period[currentTimePeriod]}</span>
+			</p>
 		</div>
 		<div class="dayContainer">
-			<img src={dateIcon} alt="" />
-			<p class="showInputContainer">May 10</p>
+			<img src={dateIcon} alt="date icon" />
+			{#if dateFocus}
+				<form on:submit={() => changeDate(true)} class="showInputContainer inputDayContainer">
+					<select bind:value={choiceMonth} class="inputMonth" on:change={makeDays}>
+						{#each Object.entries(calendar) as [month]}
+							<option value={month}>{month}</option>
+						{/each}
+					</select>
+					<select bind:value={choiceDay} class="inputDay">
+						{#each maxDay as day}
+							<option value={day}>{day}</option>
+						{/each}
+					</select>
+					<input class="saveDateBtn" type="submit" value="✓" />
+				</form>
+			{:else}
+				<p
+					class="showInputContainer date {dateFocus ? 'on' : 'off'}"
+					on:click={() => changeDate(false)}
+				>
+					{choiceMonth}
+					{choiceDay}
+					{#if !isSaveDate}
+						<pre class="warnMessage">날짜를 변경해주세요</pre>
+					{/if}
+				</p>
+			{/if}
 		</div>
 	</div>
 
 	<div class="controlDateContainer">
 		<div class="eachControlContainer">
-			<img src={upArrowIcon} alt="" />
-			<p class="controlDateText">02</p>
-			<img src={downArrowIcon} alt="" />
+			<img src={upArrowIcon} alt="" on:click={() => changeHour(1)} />
+			<p class="controlDateText">
+				{currentHour}
+			</p>
+			<img src={downArrowIcon} alt="" on:click={() => changeHour(-1)} />
 		</div>
 		<p class="colonText">:</p>
 		<div class="eachControlContainer">
-			<img src={upArrowIcon} alt="" />
-			<p class="controlDateText">00</p>
-			<img src={downArrowIcon} alt="" />
+			<img src={upArrowIcon} alt="" on:click={() => changeMinute(1)} />
+			<p class="controlDateText">{currentMinute}</p>
+			<img src={downArrowIcon} alt="" on:click={() => changeMinute(-1)} />
 		</div>
 		<div class="eachControlContainer">
-			<img class="upIcon" src={upArrowIcon} alt="" />
-			<p class="aa">PM</p>
-			<img class="downIcon" src={downArrowIcon} alt="" />
+			<img class="upIcon" src={upArrowIcon} alt="" on:click={() => changePeriod(1)} />
+			<p>{period[currentTimePeriod]}</p>
+			<img class="downIcon" src={downArrowIcon} alt="" on:click={() => changePeriod(-1)} />
 		</div>
 	</div>
 	<div class="btnContainer">
 		<button class="deleteButton" on:click|preventDefault={closePopUp}>
 			<img src={deleteIcon} alt="" />
 		</button>
-		<button class="addButton"> Save </button>
+		<button class="addButton" on:click={saveDate}> Save </button>
 	</div>
 </div>
 
@@ -79,14 +198,49 @@
 	.showInputContainer {
 		width: 100%;
 		margin-left: 10px;
-		border: 1px solid gray;
 		padding: 13px 15px;
 		border-radius: 3px;
+	}
+	.showInputContainer.time.on {
+		border: 2px solid rgb(255, 104, 71);
+	}
+	.showInputContainer.time.off {
+		border: 1px solid gray;
+	}
+
+	.showInputContainer.date.off {
+		border: 1px solid gray;
 	}
 
 	.dayContainer {
 		display: flex;
 		width: 100%;
+		position: relative;
+	}
+	.inputDayContainer {
+		border: 2px solid rgb(255, 104, 71);
+	}
+	.inputMonth,
+	.inputDay {
+		border: none;
+		background-color: transparent;
+		outline: none;
+		font-size: 18px;
+		margin-right: 5px;
+	}
+	.saveDateBtn {
+		border: 2px solid rgb(255, 104, 71);
+		color: rgb(255, 104, 71);
+		text-align: center;
+		padding: 3px 8px;
+		border-radius: 50%;
+		outline: none;
+	}
+	.warnMessage {
+		position: absolute;
+		background-color: #eee;
+		right: 20px;
+		top: 20px;
 	}
 
 	.controlDateContainer {
