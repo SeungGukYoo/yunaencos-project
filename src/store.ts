d454321b23ed39fc561e3052a01111234dc9fbeb
SeungGukYoo <dbtmd878@gmail.com>
2023-10-16 15:04:37 +0900

    feat: 더미데이터 제거

--- src/store.ts
+++ src/store.ts
@@ -9,40 +9,7 @@ interface ReservedData {
 	etc: string;
 }
 
-export const reservationList: Array<ReservedData> = [
-	{
-		name: 'David',
-		phone: '012-345-6789',
-		reservedDate: '2:00 Pm',
-		people: 4,
-		reservedTable: [7, 8],
-		etc: 'Birthday'
-	},
-	{
-		name: 'David',
-		phone: '012-345-6789',
-		reservedDate: '2:00 Pm',
-		people: 4,
-		reservedTable: [7, 8],
-		etc: ''
-	},
-	{
-		name: 'David',
-		phone: '012-345-6789',
-		reservedDate: '2:00 Pm',
-		people: 4,
-		reservedTable: [7, 8],
-		etc: ''
-	},
-	{
-		name: 'David',
-		phone: '012-345-6789',
-		reservedDate: '2:00 Pm',
-		people: 4,
-		reservedTable: [7, 8],
-		etc: ''
-	}
-];
+export const reservationList: Array<ReservedData> = [];
 
 const currentReservationInfo: ReservedData = {
 	name: '',
@@ -55,7 +22,7 @@ const currentReservationInfo: ReservedData = {
 
 const isNewAddReservation = '/';
 const isPopUpOn = false;
-
 export const handleReservationInfo = writable(currentReservationInfo);
 export const handlePopUp = writable(isPopUpOn);
 export const handleMode = writable(isNewAddReservation);
+export const handleReservationData = writable(reservationList);
