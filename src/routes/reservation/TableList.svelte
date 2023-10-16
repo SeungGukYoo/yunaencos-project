4560898ac330231f45db34e6f20994f42f5629b0
SeungGukYoo <dbtmd878@gmail.com>
2023-10-16 16:28:55 +0900

    feat: 테이블 저장을 위한 함수를 콜백으로 변경

--- src/routes/reservation/TableList.svelte
+++ src/routes/reservation/TableList.svelte
@@ -1,26 +1,13 @@
 <script lang="ts">
 	import { get } from 'svelte/store';
-	import { handleReservationInfo } from '../../store';
 
 	const allTable = Array.from({ length: 8 }, (_, idx) => `${idx + 1} Table`);
-
-	function addTalbe(idx: number) {
-		let reservedTable = get(handleReservationInfo).reservedTable;
-
-		if (reservedTable.includes(idx + 1)) {
-			alert('aleady reserved table');
-			return;
-		}
-		handleReservationInfo.update((prev) => {
-			prev.reservedTable = [...prev.reservedTable, idx + 1];
-			return prev;
-		});
-	}
+	export let addTable: (addTableNumber: number) => void;
 </script>
 
 <ul class="tableListContainer">
 	{#each allTable as table, idx}
-		<li on:click={() => addTalbe(idx)}>{table}</li>
+		<li on:click={() => addTable(idx)}>{table}</li>
 	{/each}
 </ul>
 
